e3c40cf48cc38da4dd6465ee9cdb345c70744d4a
Bondar-Sasha <aliaksandr.bondar@innowise.com>
2025-02-19 18:00:40 +0300

    fixed bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import {resolve} from 'path'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
     },
-  },
+    define: {
+      'process.env': env,
+    },
+  }
 })
